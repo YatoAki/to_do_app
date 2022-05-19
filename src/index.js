324d3f81967312a67899d8d3fa149a36cfba93ab
Yato Aki <yatoakii@gmail.com>
2022-05-19 21:44:16 +0630

    Added buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './style.css';
+import UI from "./ui.js"
+
+document.addEventListener('DOMContentLoaded', UI.loadHomePage);
---
+++ src/ui.js
@@ -0,0 +1,63 @@
+import Task from "./task.js"
+import Project from "./project.js"
+
+export default class UI{
+
+  static loadHomePage(){
+    UI.setBtns();
+  }
+
+
+  static createProject(title){
+    const para = document.createElement("p")
+    para.textContent = title;
+    return para;
+  }
+
+  static setBtns(){
+    UI.setTaskBtn();
+    UI.setProjectBtn();
+  }
+
+  static setProjectBtn(){
+    const projects = document.querySelector(".nav");
+    const addProjects = document.querySelector(".add-projects");
+    const inputField = UI.createInputField();
+    addProjects.addEventListener("click",(e)=>{
+      projects.appendChild(inputField);
+    });
+  }
+
+  static setTaskBtn(){
+    const tasks = document.querySelector(".tasks");
+    const addTasks = document.querySelector(".add-tasks");
+    const inputField = UI.createInputField();
+    addTasks.addEventListener("click",(e)=>{
+      tasks.appendChild(inputField);
+    });
+  }
+
+  static createInputField(){
+    const inputField = document.createElement("div")
+    inputField.classList.add("input-field");
+    const text = document.createElement("input");
+    text.classList.add("input")
+    text.setAttribute("type", "text");
+    const div = document.createElement("div");
+    const btnY = document.createElement("button");
+    btnY.textContent = "Yes";
+    const btnN = document.createElement("button");
+    btnN.textContent = "Cancel";
+    btnN.addEventListener("click", (e) => {
+      inputField.parentElement.removeChild(inputField);
+    });
+    btnY.classList.add("yes");
+    btnN.classList.add("no");
+    div.appendChild(btnY);
+    div.appendChild(btnN);
+    inputField.appendChild(text);
+    inputField.appendChild(div);
+    return inputField;
+  }
+
+}
